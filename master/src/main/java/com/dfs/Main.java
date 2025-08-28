dab753ed25dab9aaeaab70fdbd208a4c3a650da7
ijaasM4 <ijaas2003>
2025-08-28 21:45:27 +0530

    commited successfully

--- master/src/main/java/com/dfs/Main.java
+++ master/src/main/java/com/dfs/Main.java
@@ -1,7 +1,7 @@
-package com.dfs;
+package distributed_system.dfs.master.src.main.java.com.dfs;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
     }
 }
