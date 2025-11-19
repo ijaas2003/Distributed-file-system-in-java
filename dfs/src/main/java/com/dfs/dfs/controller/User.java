aba41b57b99b50db20df50242acf8d30f16c0eca
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-19 22:53:12 +0530

    Controller changes

---
+++ dfs/src/main/java/com/dfs/dfs/controller/User.java
@@ -0,0 +1,22 @@
+package com.dfs.dfs.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class User {
+
+  @GetMapping("/userss")
+  public ResponseEntity<Map<String, Object>> getUser() {
+    Map<String, Object> map = new HashMap<>();
+    map.put("success", "sucess");
+    return ResponseEntity.status(HttpStatus.OK).body(map);
+  }
+}
