40fb7f6a0ab101cfe9d7ac04f484ad88e79d6a59
ijaas2003 <ijaasahamad2003@gmail.com>
2025-11-21 00:06:52 +0530

    Commit redis configurations

---
+++ dfs/src/main/java/com/dfs/dfs/Configuration/RedisConfiguration.java
@@ -0,0 +1,34 @@
+package com.dfs.dfs.Configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+@Configuration
+public class RedisConfiguration {
+
+  @Value("${spring.redis.host}")
+  private String host;
+
+  @Value("${spring.redis.password}")
+  private String password;
+
+  @Value("${spring.redis.port}")
+  private int port;
+
+  @Value("${spring.redis.timeout")
+  private int timeout;
+
+  @Bean
+  public JedisPool jedisPool() {
+    JedisPoolConfig jedisPool = new JedisPoolConfig();
+    jedisPool.setMaxIdle(20);
+    jedisPool.setMinIdle(2);
+    jedisPool.setBlockWhenExhausted(true);
+
+    return new JedisPool(jedisPool, host, port, timeout, password);
+  }
+}
